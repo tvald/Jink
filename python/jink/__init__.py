f8c7713a1d1c22995dc5c8b98abb689077036af7
Tony Valderrama <tony.valderrama@outlook.com>
2010-09-22 17:56:29 -0400

    Removed hard dependency on dev system layout in the startup script.

--- python/jink/__init__.py
+++ python/jink/__init__.py
@@ -1,7 +1,2 @@
-#!/usr/bin/python
-from __future__ import absolute_import
-from jink.main import main
-
-if __name__ == '__main__':
-  import sys
-  main(*sys.argv[1:])
+# placeholder...
+# TODO: figure out what's reasonable to put here...
---
+++ python/jink/run.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from jink.main import main
+
+if __name__ == '__main__':
+  import sys
+  main(*sys.argv[1:])
