bf7535a68e7d280133cc61c937d8fee1a10e9b1a
Tony Valderrama <tony.valderrama@outlook.com>
2011-03-18 15:28:44 -0400

    add uri parsing to jink.Jink

--- python/jink/__init__.py
+++ python/jink/__init__.py
@@ -2,9 +2,29 @@
 Jink Template Framework
 author: Tony Valderrama <tvald@galadore.net>
 """
-import jink.core
+import jink.core, jink.fs, urlparse
 
 def Jink(source, sink, config):
+  # configure source for client
+  if type(source) is str:
+    uri = urlparse.urlparse(source)
+    if uri.scheme == 'file' or uri.scheme == '':
+      if uri.netloc is not '':
+        raise Exception('file:// protocol must have empty network location')
+      source = jink.fs.SourceFS(source)
+    else:
+      raise Exception("unrecognized source protocol '%s'" % uri.scheme)
+  
+  # configure sink for client
+  if type(sink) is str:
+    uri = urlparse.urlparse(sink)
+    if uri.scheme == 'file' or uri.scheme == '':
+      if uri.netloc is not '':
+        raise Exception('file:// protocol must have empty network location')
+      sink = jink.fs.SinkFS()
+    else:
+      raise Exception("unrecognized sink protocol '%s'" % uri.scheme)
+  
   return jink.core.Engine(source, sink, config)
 
 __all__ = ['Jink']
